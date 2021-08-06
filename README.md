eb8b54789c6824dfa9e62695a4ea75d07c1f2cc0
MatthewDolder <matt@dolder.net>
2021-08-06 18:27:46 -0500

    first project submission

--- README.md
+++ README.md
@@ -1,1 +1,77 @@
-# BTS_Airline_Stats
+# Project: Create a Tebleau Story
+### Matthew Dolder
+### 8/5/2021
+
+
+## Summary: 
+
+Looking at flight delay data provided by Bureau of Transportaion Statistics from 2003 and 2021, I want to show 
+which airports have the most security delays and within those airports, which airlines are most affected each year. 
+I chose a map to show the top (5-20) airports and bubbles to show the airlines and year. 
+
+
+## Design: 
+
+### First Draft (slow)
+https://public.tableau.com/views/SecurityDelaysbyAirport-Dashboard/FlightsDelayedbySecurity?:language=en-US&:retry=yes&publish=yes&:display_count=n&:origin=viz_share_link
+
+The common feedback was performance.  It is painfully slow on Tableau Public with the Raw dataset.  I performed some data wrangling to reduce the number of records and fields.  See main.py.  This improved performance drastically. 
+
+I made the 'Security Delay' field plural. 
+
+I changed the position of the 5-20 slider
+
+My youngest daughter didn't like that the same airline name shows up in multiple bubbles.  The fact that they were different years was not obvious. I changed the label to have year first and just the airline symbol second. 
+
+### Final Version
+https://public.tableau.com/views/SecurityDelaysbyAirport-DashboardpostFeedback/FlightsDelayedbySecurity?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link
+
+## Feedback: 
+My two teenage daughters provided feedback:
+
+### Daughter 1
+
+- Slider text is difficult to read
+- Responds slowly
+- Ensure that the keep only/exclude function on click works on the world map and not just the cities
+- Define security delay (Also maybe change the 'Security Delay' text on hover to 'Security Delays')
+- UI is smooth and enjoyable
+- Lots of data (That's cool)
+
+### Daughter 2
+
+- very slow at the beginning when you start working with it
+- put names onto the other airports?
+- group actual security delays all together instead of apart?
+- dont put same name on bubble, just put what year it occured so people might not be confused
+
+## Resources: 
+
+The raw data was downloaded from bts.gov.
+I choose All carriers & All airports from June 2003 to May 2021.
+https://www.transtats.bts.gov/OT_Delay/OT_DelayCause1.asp
+ 
+### References:
+- https://help.tableau.com/current/pro/desktop/en-us/sortgroup_sets_topn.htm
+- https://www.tableau.com/learn/tutorials/on-demand/-dashboard-interactivity-using-actions
+- https://docs.python.org/3/library/csv.html
+- https://docs.python.org/3/library/sqlite3.html
+- https://stackoverflow.com/questions/10522830/-how-to-export-sqlite-to-csv-in-python-without-being-formatted-as-a-list
+- https://www.sqlite.org/inmemorydb.html
+
+## Files:
+
+### README.md
+this file
+
+### 418421584_52021_5443_airline_delay_causes.RAW.zip
+Raw data dump from bts.org.  CSV format.
+
+### 418421584_52021_5443_airline_delay_causes.zip
+Wrangled data used for Tableau.  CSV format. Output from main.py.
+
+### main.py
+data wrangling
+
+### bts_search_page.png
+screenshot of query on bts.gov to download raw data. 
---
+++ main.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3.8
+### Matthew Dolder
+### 08/06/21
+### Read BTS flight data from csv, modify and write. 
+
+import csv
+import sqlite3
+
+def setupdb(dbname):
+    #https://docs.python.org/3/library/sqlite3.html
+    con = sqlite3.connect(dbname)
+
+    cur = con.cursor()
+
+    createtbl = '''create table flights (\
+                year int,month int,carrier,carrier_name varchar(100),airport char(3),airport_name varchar(100),security_delay int)'''
+    
+    cur.execute(createtbl)
+
+    createtbl = '''create table flights_tidy (\
+                year int,carrier,carrier_name varchar(100),airport char(3),airport_name varchar(100),security_delay int)'''
+
+    cur.execute(createtbl)
+    con.commit
+    cur.close()
+
+    return con
+
+def importdata(infile,con):
+
+    cur = con.cursor()
+    sqlintro = 'insert into flights (year,month,carrier,carrier_name,airport,airport_name,security_delay) values ('
+    sqloutro = ');'
+    sql = ''
+
+    #inflights = pd.read_csv(infile, header=0).to_dict()
+    #print(inflights)
+    #for f in inflights:
+    #    print(f['year'])
+    #"year"," month","carrier","carrier_name","airport","airport_name","arr_flights","arr_del15","carrier_ct"," weather_ct",
+    # "nas_ct","security_ct","late_aircraft_ct","arr_cancelled","arr_diverted"," arr_delay"," carrier_delay","weather_delay",
+    # "nas_delay","security_delay","late_aircraft_delay",
+
+    with open(infile,newline='') as csvfile:
+        inflights = csv.reader(csvfile,delimiter=',',quotechar='"')
+        i = 0
+        for f in inflights:
+            security_delay = f[19]
+            if (security_delay == 'NaN') or (security_delay == ''):
+                security_delay = '0'
+            
+            if (i > 0): #skip header
+                sql = f[0] + ',' + f[1] + ',\'' + f[2] + '\',\'' + f[3] + '\',\'' + f[4] + '\',\'' + f[5].replace('\'','') + '\',' + security_delay
+                sql = sqlintro + sql + sqloutro
+                #print (sql)
+                cur.execute(sql)
+            i += 1
+        
+    con.commit
+    cur.close()
+
+    
+def squeezedata(con):
+
+    #remove month and remove values which are zero
+
+    sql = "insert into flights_tidy (year,carrier,carrier_name,airport,airport_name,security_delay) \
+           select year,carrier,carrier_name,airport,airport_name,sum(security_delay) \
+           from flights \
+           where security_delay > 0 \
+           group by year,carrier_name,airport,airport_name"
+    
+    cur = con.cursor()
+    cur.execute(sql)
+    con.commit()
+    cur.close()
+
+def outputdata(outfile,con):
+
+    #Write flights_tidy out to csv
+    #https://stackoverflow.com/questions/10522830/how-to-export-sqlite-to-csv-in-python-without-being-formatted-as-a-list
+    cur = con.cursor()
+    cur.execute ('select year,carrier,carrier_name,airport,airport_name,security_delay from flights_tidy order by year,airport_name')
+
+    with open(outfile, 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(['year', 'carrier','carrier_name', 'airport', 'airport_name', 'security_delay'])
+        for flight in cur.fetchall():
+            writer.writerow([flight[0],flight[1],flight[2],flight[3],flight[4],flight[5]])
+    
+    f.close()
+            
+def main():
+    infile = '418421584_52021_5443_airline_delay_causes.RAW.csv'
+    outfile = '418421584_52021_5443_airline_delay_causes.csv'        
+    #db = 'security_delays_2003_2021.db'
+    #https://www.sqlite.org/inmemorydb.html
+    db = ":memory:"  
+    
+    #get database connection
+    print("setting up database\n")
+    con = setupdb(db)
+    #import from csv into database
+    print("import csv to database\n")
+    importdata(infile,con)
+    #select and group by
+    print("Processing Data\n")
+    squeezedata(con)
+    #write the table out to csv
+    print("Writing to CSV\n")
+    outputdata(outfile,con)
+    print("Bazinga.  See " + outfile + " for results\n")
+    
+                
+
+
+
+if __name__ == "__main__":
+    main()
